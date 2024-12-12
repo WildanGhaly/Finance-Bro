d5bf45f8e43e26d897ede99e74671afab2c8ad2e
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2024-12-13 02:08:00 +0700

    feat: save token

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,10 +4,24 @@ import { jwtDecode } from 'jwt-decode';
 export default function Login() {
   return (
     <GoogleLogin
-  onSuccess={credentialResponse => {
+  onSuccess={ async credentialResponse => {
     if (credentialResponse.credential) {
-      var decoded = jwtDecode(credentialResponse.credential);
-        console.log('Decoded: ', decoded);
+      const decoded = jwtDecode(credentialResponse.credential);
+      const email = (decoded as { email: string }).email;
+      try {
+        const response = await fetch('https://financebro-backend-958019176719.us-central1.run.app/firebase', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email }),
+        });
+        const data = await response.json();
+        const token = data.token;
+        localStorage.setItem('token', token);
+      } catch (error) {
+        console.error('Error:', error);
+      }
     } else {
       console.error('Credential is undefined');
     }
