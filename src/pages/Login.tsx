c35c9a190f8c5a31dddb2b0dd6c94b690aa63a0f
Hidayatullah Wildan Ghaly Buchary <wildanghaly1@gmail.com>
2024-12-13 08:48:52 +0700

    feat: add navigator after login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,35 +1,39 @@
 import { GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from 'jwt-decode';
-
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
+  const navigate = useNavigate();
+
   return (
     <GoogleLogin
-  onSuccess={ async credentialResponse => {
-    if (credentialResponse.credential) {
-      const decoded = jwtDecode(credentialResponse.credential);
-      const email = (decoded as { email: string }).email;
-      try {
-        const response = await fetch('https://financebro-backend-958019176719.us-central1.run.app/firebase', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ email }),
-        });
-        const data = await response.json();
-        const token = data.token;
-        localStorage.setItem('token', token);
-      } catch (error) {
-        console.error('Error:', error);
-      }
-    } else {
-      console.error('Credential is undefined');
-    }
-  }}
-  onError={() => {
-    console.log('Login Failed');
-  }}
-/>
-  )
+      onSuccess={async (credentialResponse) => {
+        if (credentialResponse.credential) {
+          const decoded = jwtDecode(credentialResponse.credential);
+          const email = (decoded as { email: string }).email;
+          try {
+            const response = await fetch('https://financebro-backend-958019176719.us-central1.run.app/firebase', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ email }),
+            });
+            const data = await response.json();
+            const token = data.token;
+            localStorage.setItem('authToken', token);
+
+            navigate('/home');
+          } catch (error) {
+            console.error('Error:', error);
+          }
+        } else {
+          console.error('Credential is undefined');
+        }
+      }}
+      onError={() => {
+        console.log('Login Failed');
+      }}
+    />
+  );
 }
